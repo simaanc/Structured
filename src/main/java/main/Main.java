50f15f919bdd7071ce4c5f428e044da8f2ba0a5d
simaanc <simaanc@gmail.com>
2020-01-28 08:20:26 -0800

    Created Custom Processing Version

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,13 +8,6 @@ import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        com.sun.javafx.application.PlatformImpl.startup(() -> {
-            try {
-                PApplet.main(Structured.class);
-            } catch (Exception ex) {
-                Logger.getLogger(Structured.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        });
-
+        PApplet.main(Structured.class);
     }
 }
