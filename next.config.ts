6312f01b4f7cef6ff01bdd80c91dd6697d5d96aa
simaanc <simaanc@gmail.com>
2025-04-24 18:54:44 -0700

    chore: Add basePath, assetPrefix, and trailingSlash to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  basePath: '/projects/structured/live',
+  assetPrefix: '/projects/structured/live',
+  trailingSlash: true,     // ← add this!
 };
 
 export default nextConfig;
