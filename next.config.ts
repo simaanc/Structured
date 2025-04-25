a46fcf003fcbcf1f80cc95a94e1a17987968036c
simaanc <simaanc@gmail.com>
2025-04-24 18:50:38 -0700

    chore: Remove basePath and assetPrefix from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/projects/structured/live',
-  assetPrefix: '/projects/structured/live',
   reactStrictMode: true,
 };
 
